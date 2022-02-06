4383518f27f2831a80aed14d40849498ee4968be
s-a-alex <s-a-alex@yandex.ru>
2022-02-06 15:07:07 +0300

    Initial commit

---
+++ src/test/java/getphos/spring/spring_boot_jms/SpringBootJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package getphos.spring.spring_boot_jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
